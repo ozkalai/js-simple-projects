b89483839587b24cd96c3fd0e0c06267f26fe1d5
ozkalai <melihozkalayy@gmail.com>
2021-04-30 14:34:44 +0300

    scroll updated

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
-const btn = document.querySelector(".switch-btn")
-const video = document.querySelector(".video-container")
+// ****** set date ****** 
 
-btn.addEventListener("click" , function() {
-  if(!btn.classList.contains("slide")){
-    btn.classList.add("slide")
-    video.pause();
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear()
+
+// ****** close links *******
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click" , function() {
+  // linksContainer.classList.toggle("show-links")
+  const containerHeight = linksContainer.getBoundingClientRect();
+  const linkHeight = links.getBoundingClientRect().height;
+  console.log(linkHeight) 
+  console.log(containerHeight.height) 
+
+  if(containerHeight.height === 0){
+    linksContainer.style.height = `${linkHeight}px`
   }
   else{
-    btn.classList.remove("slide")
-    video.play()
+    linksContainer.style.height = 0
   }
-} )
 
-// preloader 
+})
 
-const preloader = document.querySelector(".preloader");
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+// ******* fixed navbar ******* 
 
-window.addEventListener("load" , function() {
-  preloader.classList.add("hide-preloader");
+window.addEventListener("scroll" , function() {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  console.log(navHeight);
+
+  if(scrollHeight > navHeight){
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav")
+  }
+
+  if( scrollHeight > 500) {
+    topLink.classList.add("show-link")
+  }else{
+    topLink.classList.remove("show-link")
+  }
 })
