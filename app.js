a5c43f2285881f5fcecab6a0380081e3025ce544
ozkalai <melihozkalayy@gmail.com>
2021-04-30 16:46:55 +0300

    final tabs

--- app.js
+++ app.js
@@ -1,81 +1,21 @@
-// ****** set date ****** 
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
 
-const date = document.getElementById("date");
-date.innerHTML = new Date().getFullYear()
-
-// ****** close links *******
-
-const navToggle = document.querySelector(".nav-toggle");
-const linksContainer = document.querySelector(".links-container");
-const links = document.querySelector(".links");
-
-navToggle.addEventListener("click" , function() {
-  // linksContainer.classList.toggle("show-links")
-  const containerHeight = linksContainer.getBoundingClientRect();
-  const linkHeight = links.getBoundingClientRect().height;
-  console.log(linkHeight) 
-  console.log(containerHeight.height) 
-
-  if(containerHeight.height === 0){
-    linksContainer.style.height = `${linkHeight}px`
-  }
-  else{
-    linksContainer.style.height = 0
-  }
-
-})
-
-const navbar = document.getElementById("nav");
-const topLink = document.querySelector(".top-link");
-// ******* fixed navbar ******* 
-
-window.addEventListener("scroll" , function() {
-  const scrollHeight = window.pageYOffset;
-  const navHeight = navbar.getBoundingClientRect().height;
-  console.log(navHeight);
-
-  if(scrollHeight > navHeight){
-    navbar.classList.add("fixed-nav");
-  } else {
-    navbar.classList.remove("fixed-nav")
-  }
-
-  if( scrollHeight > 500) {
-    topLink.classList.add("show-link")
-  }else{
-    topLink.classList.remove("show-link")
-  }
-})
-
-// ******** smooth scrol l ******
-// select links 
-
-const scrollLinks = document.querySelectorAll(".scroll-link");
-
-scrollLinks.forEach(function(link) {
-  link.addEventListener("click" , function(e){
-    //prevent default
-    e.preventDefault();
-    // navigate to specific spot 
-    const id = e.currentTarget.getAttribute("href").slice(1);
-    console.log(id)
-    const element = document.getElementById(id)
-    console.log(element)
-    // calculate the heights 
-    const navHeight = navbar.getBoundingClientRect().height;
-    const containerHeight = linksContainer.getBoundingClientRect().height;
-    const fixedNav = navbar.classList.contains("fixed-nav")
-    let position = element.offsetTop - navHeight;
-    if(!fixedNav){
-      position = position - navHeight
-    }
-
-    if(navHeight > 82) {
-      position = position + containerHeight
-    }
-    window.scrollTo({
-      left:0,top: position
+about.addEventListener("click" , function(e) {
+  const id = e.target.dataset.id;
+  if(id){
+    //remove active from all btns 
+    btns.forEach(function(btn){
+      btn.classList.remove("active");
+      e.target.classList.add("active")
+    });
+    // hide other articles
+    articles.forEach(function(article) {
+      article.classList.remove("active")
     })
-    linksContainer.style.height = 0;
-  })
+    const element = document.getElementById(id);
+    console.log(element)
+    element.classList.add("active")
+  }
 })
