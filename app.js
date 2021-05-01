3a3f2dea82584c501661815e3a6707816cd93f8c
ozkalai <melihozkalayy@gmail.com>
2021-05-01 16:36:02 +0300

    final countdown

--- app.js
+++ app.js
@@ -26,7 +26,14 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4")
 
-let futureDate = new Date(2021,4,5,11,30,0) ;
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+let futureDate = new Date(tempYear , tempMonth ,tempDay + 10, 11,30,0)
+
+// let futureDate = new Date(2021,4,5,11,30,0) ;
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
